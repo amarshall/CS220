6a8751665f0699a044278acf0844ea0715e49033
Andrew Marshall <amarshall.iv@gmail.com>
2009-12-10 12:54:24 -0500

    Optimized inline assembly and fixed an issue where the last item might not always be sorted

--- Final Project/final.c
+++ Final Project/final.c
@@ -13,16 +13,13 @@ bool bubbleSortHelper(int i, char * list, bool swapped) {
 	char *var1 = &list[i];
 	char *var2 = &list[i+1];
 	__asm__(
-		"movb (%1), %%dl\n\t"
-		"cmpb %%dl, (%2)\n\t"
+		"cmpb %3, %4\n\t"
 		"jle noswap\n\t"
-		"movl $1, %0\n\t"
-		"xchgb %%dl, (%2)\n\t"
-		"movb %%dl, (%1)\n"
+		"xchgb %0, %1\n\t"
+		"movl $1, %2\n\t"
 		"noswap:\n\t"
-		:"=r" (swapped)
-		:"r" (var1), "r" (var2), "0" (swapped)
-		:"edx"
+		:"=r" (*var1), "=r" (*var2), "=r" (swapped)
+		:"0" (*var1), "1" (*var2), "2" (swapped)
 	);
 	return swapped;
 }
@@ -32,7 +29,7 @@ char* bubbleSort(char * list, int size) {
 	do {
 		swapped = false;
 		int i;
-		for(i = 0; i < size-2; i++) {
+		for(i = 0; i <= size-2; i++) {
 			swapped = bubbleSortHelper(i, list, swapped);
 		}
 		if(!swapped) break;
